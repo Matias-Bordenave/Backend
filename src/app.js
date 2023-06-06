2de54474cff4d8a2b429322cfeac45d6bafd2ec2
Matias-Bordenave <matibordenave@gmail.com>
2023-06-06 16:27:21 -0300

    agrego cambios

--- src/app.js
+++ src/app.js
@@ -7,25 +7,17 @@ const ProductManager = require('./productManager');
 const productManager = new ProductManager('./productos.json');
 
 
-app.get('/', (req, res) => {
-    if (req.query.limit) {
-        let prod_array = []
-        let i = 1
-        while (i <= parseInt(req.query.limit)) {
-            prod_array.push(cart.getProductById(i))
-            i++
-        }
-        res.json(prod_array)
-    } else {
-        res.json(cart.getProducts())
-    }
-})
-
 // Endpoint para obtener todos los productos
 app.get('/products', async (req, res) => {
     try {
-        const products = await productManager.getAllProducts();
-        res.json(products);
+        if (req.query.limit) {
+            const products = await productManager.getProductsWithLimit(parseInt(req.query.limit))
+            res.json(products);
+        } else {
+            const products = await productManager.getAllProducts();
+            res.json(products);
+        }
+        
     } catch (error) {
         console.log(error);
         res.status(500).send('Error al obtener los productos');
--- src/productManager.js
+++ src/productManager.js
@@ -3,13 +3,13 @@
 const fs = require("fs");
 
 class ProductManager {
-    constructor(patch) {
-        this.patch = patch;
+    constructor(path) {
+        this.path = path;
     }
 
     async getAllProducts() {
         try {
-            const pr = await fs.promises.readFile(this.patch, "utf-8");
+            const pr = await fs.promises.readFile(this.path, "utf-8");
             const prParse = JSON.parse(pr);
             if (prParse.length <= 0) {
                 return "No hay productos en la base de datos";
@@ -21,11 +21,29 @@ class ProductManager {
         }
     }
 
+    async getProductsWithLimit(limit) {
+        try {
+            const pr = await fs.promises.readFile(this.path, "utf-8");
+            const prParse = JSON.parse(pr);
+            if (prParse.length <= 0) {
+                return "No hay productos en la base de datos";
+            } else {
+                return prParse.filter((item) => item.id <= limit);
+            }
+        } catch (error) {
+            throw new Error("Error al leer el archivo: " + error);
+        }
+    }
+
     async getProductById(id) {
         try {
-            const pr = await fs.promises.readFile(this.patch, "utf-8");
+            const pr = await fs.promises.readFile(this.path, "utf-8");
+
+            //Trae todos los productos
             const prParse = JSON.parse(pr);
-            const product = prParse.find((ele) => ele.id === id);
+
+            const product = prParse.find((ele) => ele.id == id);
+
             if (product) {
                 return product;
             } else {
@@ -35,6 +53,56 @@ class ProductManager {
             throw new Error("Error al leer el archivo: " + error);
         }
     }
-}
 
-module.exports = ProductManager;
+    async addProduct(product) {
+        await this.getAllProducts().then(async (res) => {
+
+            let codes = res.filter((item) => item.code == product.code)
+
+            if (codes.length == 0) {
+                res.push(product)
+                console.log(res)
+                await fs.promises.writeFile(this.path, JSON.stringify(res), { encoding: 'utf-8' }).then(
+                    (res) => {
+                        console.log("Productos cargados correctamente")
+                    }
+                ).catch((err) => {
+                    console.log("Error guardando el producto: ", err)
+                })
+            } else {
+                console.log("El producto con el codigo ingresado ya se encuentra cargado")
+            }
+        })
+    }
+
+    async updateProduct(id, infoNew) {
+        let pr = await fs.promises.readFile(this.path, 'utf-8').then((res) => { })
+        let prParse = JSON.parse(pr)
+
+        let arrayUpdated = prParse.map((ele) => {
+            if (ele.id == id) {
+                return { ...ele, title: infoNew.title, price: infoNew.price }
+
+            } else {
+                return ele
+            }
+        })
+
+        await fs.promises.writeFile(this.path, JSON.stringify(arrayUpdated, null, 2), 'utf-8')
+
+    }
+}
+/* 
+module.exports = ProductManager;  */
+
+const cart = new ProductManager('../productos.json')
+
+cart.addProduct({
+    "id": "33",
+    "code": "aaaaa123",
+    "title": "prodaaaaucto prueba2",
+    "description": "Esaaate es un producto prueba2",
+    "thumbnail": "Sin imaaaagen2",
+    "price": 242,
+    "stock": 25
+})
